f7edb6c164fb769e33955a945aec72b0ea36585e
oscardperez26 <odanieloperez26@hotmail.com>
2026-01-16 10:47:25 -0500

    Ajustes backend: configuración y conexión DB 1

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,11 @@
  * Servicio para consumir la API del backend.
  */
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://permoda.onrender.com/api/products';
 
 export const getProductBycod = async (ean: string) => {
   try {
-    const response = await fetch(`${BASE_URL}/products/${ean}`);
+    const response = await fetch(`${BASE_URL}/${ean}`);
     if (!response.ok) return null;
     return await response.json();
   } catch (error) {
