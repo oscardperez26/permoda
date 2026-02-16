5a69b43c33dd269013cc88646e9a0397f33d0537
oscardperez26 <odanieloperez26@hotmail.com>
2026-02-16 16:07:45 -0500

    Última actualización

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // 🔹 Tus datos de Supabase
 const SUPABASE_URL = 'https://nhqwwqbbuyomvtnnqcpu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocXd3d...';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocXd3cWJidXlvbXZ0bm5xY3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNjg2MjQsImV4cCI6MjA4Njg0NDYyNH0.XuG2eHHQOzGvrj7rMnYaGT-IUlGHQ0mXtQ0rIbFeebI';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
